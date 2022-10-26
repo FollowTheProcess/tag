77857ad94ef5eb1039d1db307d300e1575ca926b
Tom Fleet <tomfleet2018@gmail.com>
2022-10-26 18:25:41 +0100

    Test against the alt toml array syntax (#29)

--- config/config_test.go
+++ config/config_test.go
@@ -38,6 +38,36 @@ func TestLoad(t *testing.T) {
 	}
 }
 
+func TestLoadAlt(t *testing.T) {
+	path := makeAltConfigFile(t)
+
+	got, err := config.Load(path)
+	if err != nil {
+		t.Fatalf("config.Load returned an error: %v", err)
+	}
+
+	want := &config.Config{
+		Tag: config.Tag{
+			Files: []config.File{
+				{
+					Path:    "hello.go",
+					Search:  "version {{.Current}}",
+					Replace: "version {{.Next}}",
+				},
+				{
+					Path:    "another.go",
+					Search:  "version {{.Current}}",
+					Replace: "version {{.Next}}",
+				},
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %#v, wanted %#v", got, want)
+	}
+}
+
 func TestRender(t *testing.T) {
 	conf := &config.Config{
 		Tag: config.Tag{
@@ -80,6 +110,28 @@ func TestRender(t *testing.T) {
 
 // makeConfigFile creates a temporary config file, returning it's path.
 func makeConfigFile(t *testing.T) string {
+	file, err := os.CreateTemp("", "tag.toml")
+	if err != nil {
+		t.Fatalf("CreateTemp returned an error: %v", err)
+	}
+	defer file.Close()
+	doc := `
+	[tag]
+	files = [
+		{ path = "hello.go", search = "version {{.Current}}", replace = "version {{.Next}}"},
+		{ path = "another.go", search = "version {{.Current}}", replace = "version {{.Next}}"},
+	]`
+	_, err = file.WriteString(doc)
+	if err != nil {
+		t.Fatalf("Could not write to tmp file: %v", err)
+	}
+
+	return file.Name()
+}
+
+// makeAltConfigFile creates a temporary config file with alternative TOML syntax
+// returning it's path.
+func makeAltConfigFile(t *testing.T) string {
 	file, err := os.CreateTemp("", "tag.toml")
 	if err != nil {
 		t.Fatalf("CreateTemp returned an error: %v", err)
